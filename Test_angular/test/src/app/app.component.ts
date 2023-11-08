682c9283f8d39cbcfc571f65e7abcfed5c9a810c
aodlichao2012-lnw <52318749+aodlichao2012-lnw@users.noreply.github.com>
2023-11-08 10:29:25 +0700

    6

--- Test_angular/test/src/app/app.component.ts
+++ Test_angular/test/src/app/app.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   title = 'test';
+  arary: string[] = [];
+  ngOnInit(){
+    this.arary.push("ssss","3333333","666666") 
+  }
 }
